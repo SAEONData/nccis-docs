b4ca7d72fbafc5d77cf59826041e5313ddb3745b
dcugh <dianauys@gmail.com>
2020-02-26 15:22:15 +0200

    fix api page

--- website/website/website/siteConfig.js
+++ website/website/website/siteConfig.js
@@ -56,7 +56,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'api', label: 'API'},
     {page: 'doc3', label: 'Dependencies'},
     // {blog: true, label: 'Changelog'},
   ],
